b0244bebfb6d91bc842ff1a352f54c4fdb8004f7
Marcus Vinicius Nobre Carraschi <marcuscarraschy@gmail.com>
2021-09-28 14:55:52 -0300

    Add functions : Remove task operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public todos: any[] = []; //Empty array list
+  public todos: Todo[] = []; //Component Todo
   public title: String = 'My tasks';
 
   constructor() {
-    this.todos.push('Task 01'); //class scope
-    this.todos.push('Task 02');
-    this.todos.push('Task 03');
-    this.todos.push({ message: 'teste' });
-    this.todos.push(new Date());
+    this.todos.push(new Todo(1, 'Task01', false));
+    this.todos.push(new Todo(2, 'Task02', false));
+    this.todos.push(new Todo(3, 'Task03', true));
   }
 
   changeText() {
     this.title = 'Test';
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1); // Remove one register
+    }
+  }
+
+  markAsDone() {
+
+  }
+
+  maskAsUndone() {
+
   }
 }
